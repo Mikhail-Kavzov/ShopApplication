30652e71b6f45162eeabe37d55b7f917c1735f75
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-14 20:54:51 +0300

    add ProductRepository implementation

---
+++ ShopApplication/Repository/Implementation/AbstractRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShopApplication.Context;
+using ShopApplication.Repository.Interfaces;
+
+namespace ShopApplication.Repository.Implementation
+{
+    public abstract class AbstractRepository:IRepository
+    {
+        protected readonly ApplicationContext _context;
+
+        protected AbstractRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ ShopApplication/Repository/Implementation/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApplication.Context;
+using ShopApplication.Models;
+using ShopApplication.Repository.Interfaces;
+
+namespace ShopApplication.Repository.Implementation
+{
+    public class ProductRepository : AbstractRepository, IProductRepository
+    {
+        public ProductRepository(ApplicationContext context) : base(context)
+        {
+        }
+
+        public void Create(Product item)
+        {
+            _context.Products.Add(item);
+        }
+
+        public void Delete(Product item)
+        {
+            _context.Products.Remove(item);
+        }
+
+        public async Task<IEnumerable<Product>> GetItemsAsync(int startPosition, int count)
+        {
+            return await _context.Products.Include(p => p.User)
+                .Skip(startPosition).Take(count).ToListAsync();
+        }
+
+        public void Update(Product item)
+        {
+            _context.Products.Update(item);
+        }
+    }
+}
