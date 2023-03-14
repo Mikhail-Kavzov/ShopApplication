8fae045d7734eda56cf65e0988decf41fbc49c53
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-14 23:30:02 +0300

    add AccessExtension for Security

---
+++ ShopApplication/Extensions/AccessExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApplication.Extensions
+{
+    public static class AccessExtension
+    {
+        public static bool HasAccess(this ControllerBase cb, string name)
+        {
+            return cb.User.IsInRole("Admin") || cb.User.Identity!.Name == name;
+        }
+    }
+}
