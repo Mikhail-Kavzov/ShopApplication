69acc4410b9378463f790cce38db2e2aa8f98d8b
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-14 23:32:14 +0300

    add update, pagination services

--- ShopApplication/Services/Interfaces/IProductService.cs
+++ ShopApplication/Services/Interfaces/IProductService.cs
@@ -1,9 +1,15 @@
-﻿using ShopApplication.ViewModel;
+﻿using ShopApplication.Models;
+using ShopApplication.ViewModel;
 
 namespace ShopApplication.Services.Interfaces
 {
     public interface IProductService : IService
     {
-        void Create(ProductViewModel model);
+        void Create(ProductViewModel model, string userId);
+        Task<Product?> GetProductById(int id);
+        ProductViewModel CreateProductViewModel(Product product);
+        void Update(Product product, ProductViewModel model);
+        Task<IEnumerable<Product>> GetProductsAsync(int startPosition, int count);
+        Task<int> ProductsTotal();
     }
 }
