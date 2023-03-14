2a6c7b2a454d656bcf1baefac2f4c2467a6bbfce
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-14 18:34:18 +0300

    Add DatabaseContext

---
+++ ShopApplication/Context/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopApplication.Models;
+
+namespace ShopApplication.Context
+{
+    public class ApplicationContext:IdentityDbContext<User>
+    {
+        public DbSet<User> User { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
