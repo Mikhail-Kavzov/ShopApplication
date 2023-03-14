d87170977fc93abe9fff729fd7b0edbb14a87250
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-14 20:56:21 +0300

    add ProductViewModel

---
+++ ShopApplication/ViewModel/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopApplication.ViewModel
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        public int Price { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string? PhotoLink { get; set; }
+    }
+}
