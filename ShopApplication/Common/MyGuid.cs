49f972ad57c8529f0c9519bac7278840ca318cec
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-14 20:53:07 +0300

    add own guid algorithm (for file names)

---
+++ ShopApplication/Common/MyGuid.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace ShopApplication.Common
+{
+    public static class MyGuid
+    {
+        public static string GetGuid()
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+        }
+    }
+}
