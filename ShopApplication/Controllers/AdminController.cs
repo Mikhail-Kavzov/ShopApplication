f4d83f204476a417c123842d3598d33d59fec3f9
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-15 01:27:20 +0300

    add admin controller

---
+++ ShopApplication/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApplication.Controllers
+{
+    [Authorize(Roles ="admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
