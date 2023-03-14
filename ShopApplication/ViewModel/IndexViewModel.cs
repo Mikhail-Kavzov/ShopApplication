32e1802c2098a52720454939b19b11eb31124143
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-14 23:32:38 +0300

    add ViewModels for pagination

---
+++ ShopApplication/ViewModel/IndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopApplication.ViewModel
+{
+    public class IndexViewModel<T> where T : class
+    {
+        public PageInfoViewModel PageInfo { get; set; } = null!;
+        public IEnumerable<T> Items { get; set; } = null!;
+    }
+}
--- ShopApplication/ViewModel/ProductViewModel.cs
+++ ShopApplication/ViewModel/ProductViewModel.cs
@@ -1,12 +1,17 @@
-﻿namespace ShopApplication.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopApplication.ViewModel
 {
     public class ProductViewModel
     {
         public int ProductId { get; set; }
 
         public int Price { get; set; }
+
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public string? PhotoLink { get; set; }
+
+        public string? UserName { get; set; }
     }
 }
