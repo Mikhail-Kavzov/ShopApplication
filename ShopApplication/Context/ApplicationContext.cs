9c10be4c50cb55ef295e32bbf7f37472fc6b360d
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-15 13:15:20 +0300

    remove Users from context

--- ShopApplication/Context/ApplicationContext.cs
+++ ShopApplication/Context/ApplicationContext.cs
@@ -6,7 +6,6 @@ namespace ShopApplication.Context
 {
     public class ApplicationContext : IdentityDbContext<User>
     {
-        public DbSet<User> User { get; set; } = null!;
         public DbSet<Product> Products { get; set; } = null!;
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
