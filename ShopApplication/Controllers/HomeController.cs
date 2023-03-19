855b1b480024747f52347c6fad18768b96336cd5
Mikhail-Kavzov <kavzovm@mail.ru>
2023-03-19 13:28:26 +0300

    add SelectLanguge action from cookie

--- ShopApplication/Controllers/HomeController.cs
+++ ShopApplication/Controllers/HomeController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ShopApplication.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1),
+                    IsEssential = true }
+            );
+            return LocalRedirect(returnUrl);
+        }
     }
+
+    
 }
